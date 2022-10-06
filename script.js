4efa50e3d18a93a1f06b4798be51fe070d9dc4a6
Eliseu <eliseu.imasd@gmail.com>
2022-10-05 21:07:44 -0300

    Implementando Contador de Pontos com Botão reset de partida

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const tile = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
+    const resetGame = document.querySelector('#game-reset')
+    const playerXWin = document.querySelector('#playerX')
+    const player0Win = document.querySelector('#player0')
+    const playersWon = document.querySelector('#won')
+
+    let count1 = 1, count2 = 1, countWon = 1;
 
     let board = ['','','','','','','','',''];
     let currentPlayer = 'X';
@@ -59,12 +65,18 @@ window.addEventListener('DOMContentLoaded', () => {
         switch(type){
             case PLAYER0_WON:
                 announcer.innerHTML = 'Player <span class="player0">0</span> Won';
+                player0Win.innerText = `Jogador 0: ${count2}`;
+                count2++;
                 break;
             case PLAYERX_WON:
                 announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                playerXWin.innerText = `Jogador X: ${count1}`;
+                count1++;
                 break;
             case TIE:
                 announcer.innerText = 'Tie';
+                playersWon.innerHTML = `<span>Empates: ${countWon}</span>`
+                countWon++
         }
         announcer.classList.remove('hide');
     };
@@ -113,9 +125,29 @@ window.addEventListener('DOMContentLoaded', () => {
             tile.classList.remove('player0');
         });
     }
+    const resetEveryTable = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+        
+        if(currentPlayer === '0'){
+            changePlayer();
+        }
 
-    tiles.forEach( (tile, index) => {
+        tile.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('player0');
+        });
+
+        player0Win.innerText = ''
+        playerXWin.innerText = ''
+        playersWon.innerText = ''
+    }
+
+    tile.forEach( (tile, index) => {
         tile.addEventListener('click', () => userAction(tile, index));
     });
     resetButton.addEventListener('click', resetBoard);
+    resetGame.addEventListener('click', resetEveryTable)
 });
